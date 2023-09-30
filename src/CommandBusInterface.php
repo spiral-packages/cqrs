5c4732339080a799f13dfcd6248dcc98fd9a1752
butschster <butschster@gmail.com>
2023-09-30 20:33:54 +0400

    Add Psalm annotations to query and command buses to provide type information for return values

--- src/CommandBusInterface.php
+++ src/CommandBusInterface.php
@@ -6,5 +6,11 @@ namespace Spiral\Cqrs;
 
 interface CommandBusInterface
 {
+    /**
+     * @template TResult
+     * @param CommandInterface<TResult> $command
+     *
+     * @return TResult
+     */
     public function dispatch(CommandInterface $command): mixed;
 }
